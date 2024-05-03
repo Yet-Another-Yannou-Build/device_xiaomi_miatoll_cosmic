4396595f469bf6034c59ce1b9c48a1c4a6ab16c9
Yann <yannrenard1025@gmail.com>
2024-05-03 18:37:06 +0200

    chore: Remove unused inheritance from lineage_miatoll.mk

--- lineage_miatoll.mk
+++ lineage_miatoll.mk
@@ -19,9 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
